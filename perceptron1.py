afeb47eb21b00a78b09466962eda54753f79c2b8
Lucas Batista <124750301+LucasBatista37@users.noreply.github.com>
2023-10-05 22:03:26 -0300

    Add files via upload

--- perceptron1.py
+++ perceptron1.py
@@ -1,17 +1,12 @@
-entradas = [1, 7, 5]
-pesos = [0.8, 0.1, 0]
+#Importar a bibliteca numpy para usar na função soma e usado para lidar com grandes quantidades de dados além de deixar o código mais limpo 
+
+import numpy as np
+
+entradas = np.array([1, 7, 5])
+pesos = np.array([0.8, 0.1, 0])
 
 def soma(e, p):
-    s = 0
-    #o loop sabe que deve itear sobre os dois vetores, já que foi passado os argunmentos para a função soma
-    for i in range(3):  
-        print(entradas[i])
-        print(pesos[i])
-
-        s += e[i] * p[i]
-
-    print(s)
-    return s
+    return e.dot(p)
 
 #tornar o resultado disponivel caso ele precise ser utilizado para outras funções
 s = soma(entradas, pesos)
